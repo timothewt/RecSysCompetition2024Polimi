6d80761d13bf56bcdefb4a5eabdfeeca643f4cb3
Timothe Watteau <timothe.wt@gmail.com>
2024-11-04 10:56:14 +0100

    top popular recommender

---
+++ src/utils.py
@@ -0,0 +1,43 @@
+import os
+
+import numpy as np
+import pandas as pd
+import scipy.sparse as sp
+
+
+class RecommenderModel:
+	def __init__(self):
+		pass
+
+	def fit(self, *args) -> None:
+		raise NotImplementedError
+
+	def recommend(self, user_id: int, *args):
+		raise NotImplementedError
+
+
+def open_data() -> tuple[sp.csr_matrix, sp.csr_matrix]:
+	train = pd.read_csv("../data/data_train.csv")
+	ICM_metadata = pd.read_csv("../data/data_ICM_metadata.csv")
+	urm = sp.coo_matrix((train['data'], (train['user_id'], train['item_id']))).tocsr()
+	icm = sp.coo_matrix((ICM_metadata['data'], (ICM_metadata['item_id'], ICM_metadata['feature_id']))).tocsr()
+
+	return urm, icm
+
+
+def get_target_users_test() -> np.ndarray:
+	return pd.read_csv("../data/data_target_users_test.csv",).to_numpy().ravel()
+
+
+def make_submission(trained_model: RecommenderModel, filename: str = "submission.csv") -> None:
+	target_users_test = get_target_users_test()
+	recommendations = np.array([
+			trained_model.recommend(user_id) for user_id in target_users_test
+	])
+	# check if submissions folder exists
+	if not os.path.exists("../submissions"):
+		os.makedirs("../submissions")
+	with open(f"../submissions/{filename}", "w") as f:
+		f.write("user_id,item_list\n")
+		for user_id, recs in zip(target_users_test, recommendations):
+			f.write(f"{user_id},{' '.join(map(str, recs))}\n")
