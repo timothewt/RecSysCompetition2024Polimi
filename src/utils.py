bed39b88076d56de967ba5523875b5fa0ea54fba
timothewt <timothe.wt@gmail.com>
2024-11-19 00:41:45 +0100

    Matrix Factorization with MSE

--- src/utils.py
+++ src/utils.py
@@ -21,7 +21,7 @@ def open_dataset() -> tuple[sp.csr_matrix, sp.csr_matrix]:
 	return urm, icm
 
 
-def train_test_split(urm: sp.csr_matrix, test_size: float = .20) -> tuple[sp.csr_matrix, sp.csr_matrix]:
+def train_test_split(urm: sp.csr_matrix, test_size: float = .2) -> tuple[sp.csr_matrix, sp.csr_matrix]:
 	"""Splits the URM matrix into a train and test dataset over the users.
 
 	:param urm: The User-Rating matrix
@@ -61,7 +61,7 @@ def average_precision(recommendations: np.ndarray, y: np.ndarray, k: int = 10) -
 	return np.sum(precisions * relevance_mask) / min(len(y), k) if len(y) > 0 else 0.
 
 
-def evaluate_model(trained_model: RecommenderModel, urm_test: sp.csr_matrix, at: int = 10) -> float:
+def evaluate_model(trained_model: RecommenderModel, urm_test: sp.csr_matrix, at: int = 10, users_to_test: float = 1.) -> float:
 	"""Evaluates a recommender model using the MAP metric
 
 	:param trained_model: A fitted recommender model
@@ -70,13 +70,18 @@ def evaluate_model(trained_model: RecommenderModel, urm_test: sp.csr_matrix, at:
 	:type urm_test: sp.csr_matrix
 	:param at: The number of items to recommend to each user
 	:type at: int
+	:param users_to_test: The ratio of users to test (in [0,1])
+	:type users_to_test: float
 	:return: The MAP metric for this model on this test data
 	:rtype: float
 	"""
 	cum_ap = 0.
 	eval_count = 0
 
-	for user_id in range(urm_test.shape[0]):
+	num_users = urm_test.shape[0]
+	users_ids = np.arange(num_users) if users_to_test == 1 else np.random.choice(num_users, size=int(users_to_test * num_users))
+
+	for user_id in users_ids:
 		y = urm_test.indices[urm_test.indptr[user_id]:urm_test.indptr[user_id+1]]
 		if len(y) > 0:
 			eval_count += 1
@@ -107,7 +112,7 @@ def train_model(model: RecommenderModel, at: int = 10, test_size: float = .2, pr
 	model.fit(urm_train, icm, urm_test, **kwargs)
 
 	if print_eval and test_size > 0:
-		print(f"MAP@{at} evaluation of the {model.__class__.__name__} model: {evaluate_model(model, urm_test, at=at):.5f}")
+		print(f"MAP@{at} evaluation of the {model.__class__.__name__} model: {evaluate_model(model, urm_test, at=at, users_to_test=.2):.5f}")
 
 	return model
 
@@ -152,7 +157,7 @@ def tf_idf(mat: sp.csr_matrix) -> sp.csr_matrix:
 	return mat
 
 
-def plot_losses(epochs: int, loss_history: np.ndarray | list, loss_history_val: np.ndarray | list, num_batch_per_epochs: int) -> None:
+def plot_losses(epochs: int, loss_history: np.ndarray | list, loss_history_val: np.ndarray | list, num_batch_per_epochs: int, other_data: tuple = None) -> None:
 	"""Plots the losses history of a training.
 
 	:param epochs: The number of epochs
@@ -163,12 +168,20 @@ def plot_losses(epochs: int, loss_history: np.ndarray | list, loss_history_val: 
 	:type loss_history_val: np.ndarray | list
 	:param num_batch_per_epochs: The number of batches per epoch
 	:type num_batch_per_epochs: int
+	:param other_data: Other data to plot (optional). The format is (label: str, x: list, y: list)
+	:type other_data: tuple
 	"""
 	plt.plot(loss_history, label="Train loss")
 	plt.plot([x * num_batch_per_epochs for x in range(epochs + 1)], loss_history_val, label="Validation loss")
 	plt.xlabel("Train iteration")
 	plt.ylabel("Loss")
 	plt.title("Loss history")
-	plt.legend(loc="best")
+	plt.legend(loc="upper right")
+	if other_data:
+		label, x, y = other_data
+		ax2 = plt.gca().twinx()
+		ax2.plot(x, y, label=label, c="C2")
+		plt.legend(loc="lower left")
+
 	plt.grid(True)
 	plt.show()
