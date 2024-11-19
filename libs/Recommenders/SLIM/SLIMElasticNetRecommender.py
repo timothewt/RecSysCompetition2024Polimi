7244d376dc107e2680132d3e883812a5e79f3795
timothewt <timothe.wt@gmail.com>
2024-11-19 15:16:33 +0100

    few MF models (warp still not tried)

--- libs/Recommenders/SLIM/SLIMElasticNetRecommender.py
+++ libs/Recommenders/SLIM/SLIMElasticNetRecommender.py
@@ -7,10 +7,10 @@
 
 import numpy as np
 import scipy.sparse as sps
-from Recommenders.Recommender_utils import check_matrix
+from libs.Recommenders.Recommender_utils import check_matrix
 from sklearn.linear_model import ElasticNet
-from Recommenders.BaseSimilarityMatrixRecommender import BaseItemSimilarityMatrixRecommender
-from Recommenders.Similarity.Compute_Similarity_Python import Incremental_Similarity_Builder
+from libs.Recommenders.BaseSimilarityMatrixRecommender import BaseItemSimilarityMatrixRecommender
+from libs.Recommenders.Similarity.Compute_Similarity_Python import Incremental_Similarity_Builder
 from libs.Utils.seconds_to_biggest_unit import seconds_to_biggest_unit
 import time, sys
 from tqdm import tqdm
